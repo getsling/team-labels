368d2edf51dc5cabaeecccc3987f1b1345bb6ebf
Rishi Ramraj <thereisnocowlevel@gmail.com>
2021-11-14 16:35:11 -0500

    Fix pre-commit

--- src/utils.ts
+++ src/utils.ts
@@ -4,12 +4,12 @@ export function get_label_names(labels: Labels): LabelNames {
   /*
    * Extract label names from label objects.
    */
-  return new Set(labels.map((label) => label.name));
+  return new Set(labels.map((label: Labels[number]) => label.name));
 }
 
 export function get_assignee_logins(assignees: Assignees): AssigneeLogins {
   /*
    * Extract assignee logins from assignee objects.
    */
-  return new Set(assignees.map((assignee) => assignee.login));
+  return new Set(assignees.map((assignee: Assignees[number]) => assignee.login));
 }
