6e81f7c678b467ae70416c0c08d12bb2c83dd381
Rishi Ramraj <thereisnocowlevel@gmail.com>
2021-11-14 19:49:30 -0500

    Add set tests

--- src/set.ts
+++ src/set.ts
@@ -14,7 +14,7 @@ export function intersection(a: Set<any>, b: Set<any>): Set<any> {
 
 export function difference(a: Set<any>, b: Set<any>): Set<any> {
   /*
-   * Returns a set containing a \ b.
+   * Returns a set containing a - b.
    */
   return new Set([...a].filter((x) => !b.has(x)));
 }
