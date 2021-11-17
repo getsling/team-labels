113dadfdb06be30be4319c7138f22118297e36f7
Rishi Ramraj <thereisnocowlevel@gmail.com>
2021-11-17 16:55:38 -0500

    Move get_new_labels to labels.ts

--- src/labels.ts
+++ src/labels.ts
@@ -1,5 +1,21 @@
-import { LabelNames } from './types';
+import { LabelNames, Labels, Assignees, Config } from './types';
+import { get_assignee_logins, get_label_names } from './utils';
+import { union, difference } from './set';
 import { Context } from 'probot';
+
+export function get_new_labels(assignees: Assignees, labels: Labels, config: Config): LabelNames {
+  /*
+   * Get a set of new labels for the issue.
+   */
+  // Determine which labels need to be added.
+  let new_labels = new Set();
+  for (let assignee of Array.from(get_assignee_logins(assignees))) {
+    new_labels = union(new_labels, config[assignee] ?? new Set());
+  }
+
+  // Remove any existing labels.
+  return difference(new_labels, get_label_names(labels));
+}
 
 export async function add(context: Context, labels: LabelNames): Promise<void> {
   /*
--- test/labels.test.ts
+++ test/labels.test.ts
@@ -1,8 +1,14 @@
-import { add } from '../src/labels';
+import { add, get_new_labels } from '../src/labels';
 import { Context } from 'probot';
+import { Config } from '../src/types';
+import issue from './fixtures/issue.json';
 
 describe('labels', () => {
   let context: Context;
+  const config: Config = {
+    alice: new Set(['frontend']),
+    bob: new Set(['frontend', 'full stack'])
+  };
 
   beforeEach(async () => {
     // Create a mock context.
@@ -17,8 +23,23 @@ describe('labels', () => {
     } as any;
   });
 
+  test.each([
+    ['assignees and labels', issue.assignees, issue.labels, new Set(['full stack'])],
+    ['labels only', [], issue.labels, new Set()],
+    ['assignees only', issue.assignees, [], new Set(['frontend', 'full stack'])]
+  ])('get_new_labels with %s', (name, assignees, labels, expected) => {
+    // Suppress the undefined name.
+    expect(name);
+
+    // Run the test.
+    let result = get_new_labels(assignees, labels, config);
+
+    // Check the result.
+    expect(result).toEqual(expected);
+  });
+
   test('labels are added', async () => {
-    // Monitor the log.
+    // Monitor addLabels.
     const spy = jest.spyOn(context.octokit.issues, 'addLabels');
 
     // Run the test.
@@ -28,5 +49,17 @@ describe('labels', () => {
     // Check the result.
     let expected = { labels: Array.from(labels) };
     expect(spy.mock.calls[0][0]).toEqual(expected);
+  });
+
+  test('add exits early', async () => {
+    // Monitor addLabels.
+    const spy = jest.spyOn(context.octokit.issues, 'addLabels');
+
+    // Run the test.
+    let labels = new Set([]);
+    await add(context, labels);
+
+    // Check the result.
+    expect(spy.mock.calls.length).toEqual(0);
   });
 });
